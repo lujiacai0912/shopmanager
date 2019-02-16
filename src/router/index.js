32cd7ea78eec585b323841561c6137964e82c841
lujiacai <2824000340@qq.com>
2019-02-17 01:24:57 +0800

    登陆的路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+// import Login from '../components/login';
+
+import Login from '@/components/login';
+
 Vue.use(Router);
+// Vue.use(Login);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-    },
-  ],
+  routes: [{
+    path: '/',
+    name: 'home',
+  }, {
+    name: 'login',
+    path: '/login',
+    component: Login,
+  }],
 });
