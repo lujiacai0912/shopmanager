4746bd01ba1ef97eac88084059e1b77f483a4445
lujiacai <2824000340@qq.com>
2019-02-16 23:31:54 +0800

    ces

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+//开发模式
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"'
+})
---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+// 生产模式
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"'
+}
+
